7891c7d529b8c1dab082ed4c22d59881c01f725a
Paweł Białek <pawelbialek23@op.pl>
2018-02-10 16:47:58 +0100

    Initial commit

---
+++ komentarze/js/1-comments.js
@@ -0,0 +1,6 @@
+/*komentarz blokowy
+Dzięki niemu możemy nasz komentarz umieścić w wielu liniach
+Skrót klawiaturowy CTRL + SHIFT + /*/
+
+// komnentarz liniowy, skończy się razem z końcem tego wiersza, skrót CTRL + /
+
---
+++ zmienne/js/2-var.js
@@ -0,0 +1,19 @@
+var imie = "Adam";
+var nazwisko = "Nowacki";
+var wiek = 35;
+var wzrost;
+
+//var wynik = "Imię: " + imie + " Nazwisko: " + nazwisko + " Wiek: " + wiek;
+//console.log(imie);
+//console.log(nazwisko);
+//console.log(wiek);
+//console.log(wynik);
+//console.log(wzrost);
+
+function pokazDane() {
+    var imie = "Maciej"; 
+       console.log(imie);
+}
+
+console.log(imie);
+pokazDane();
