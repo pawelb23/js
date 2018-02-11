c6938daaa85e2c3f99b8fcbec1f738a6efd8abd5
Paweł Białek <pawelbialek23@op.pl>
2018-02-11 13:52:26 +0100

    obiekt math

---
+++ 8-obiekt-math/js/js.js
@@ -0,0 +1,9 @@
+console.log(Math.LN10);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.sin(20));
+console.log(Math.pow(4,2));
+console.log(Math.random());
+console.log(Math.ceil(1.75));
+console.log(Math.floor(1.49));
+console.log(Math.round(1.50));
