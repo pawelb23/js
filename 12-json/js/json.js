138635fe817ce7f6657d6d0fae5806cd566afa88
Paweł Białek <pawelbialek23@op.pl>
2018-02-12 14:25:53 +0100

    js poprawiona

--- 12-json/js/json.js
+++ 12-json/js/json.js
@@ -36,8 +36,8 @@ console.log(jsonOsoby);
 jsonOsoby.osoby.forEach( function( element, index ) {
                         console.log( element.imie );
 console.log( element.nazwisko );
-console.log( element.wzrost )
-    console.log(element.zainteresowania );
+console.log( element.wzrost );
+    console.log( element.zainteresowania );
     
     element.zainteresowania.forEach( function( z, ind ) {
         console.log( z );
