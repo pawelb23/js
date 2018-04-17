ee4573f810ef5702c7b76e1ec06976fcbe449d80
Paweł Białek <pawelbialek23@op.pl>
2018-04-17 22:40:16 +0200

    komentarze dodanie 'use strict'

--- 1-komentarze/js/komentarze.js
+++ 1-komentarze/js/komentarze.js
@@ -1,6 +1,6 @@
+'use strict';
 /*komentarz blokowy
 Dzięki niemu możemy nasz komentarz umieścić w wielu liniach
 Skrót klawiaturowy CTRL + SHIFT + /*/
 
 // komnentarz liniowy, skończy się razem z końcem tego wiersza, skrót CTRL + /
-
