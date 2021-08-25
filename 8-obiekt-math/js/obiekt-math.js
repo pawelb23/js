b3f5d8045882848e3c66639eb1bd02b05bc3585b
Paweł Białek <pawelbialek23@op.pl>
2021-08-25 21:42:51 +0200

    Aktualizacja

--- 8-obiekt-math/js/obiekt-math.js
+++ 8-obiekt-math/js/obiekt-math.js
@@ -1,18 +1,23 @@
-'use strict';
+"use strict";
+
 console.log(Math.LN10);
 console.log(Math.PI);
 console.log(Math.SQRT2);
 console.log(Math.sin(20));
-console.log(Math.pow(4,2));
+console.log(Math.pow(4, 2));
 console.log(Math.random());
 console.log(Math.ceil(1.75));
 console.log(Math.floor(1.49));
-console.log(Math.round(1.50));
+console.log(Math.round(1.5));
+
+console.log("");
+
+console.log(2 ** 5); //2 do potęgi 4
+console.log(Math.pow(2, 5)); // również 2 do potęgi 4
 
 //------------------------------------
 
 //A więc do dzieła, kilka przykładów jak liczyć modulo
-
 
 //10%5 = 0 bo 5*2+0=10
 //10%3 = 1 bo 3*3+1=10
@@ -22,4 +27,6 @@ console.log(Math.round(1.50));
 //1%2 = 1 bo 0*2+1=1
 //3%15 = 3 bo 0*15+3=3
 
-console.log('Aby nie wywalało błędu wszystkie informacje do modulo muszą być wyświetlane jako komentarz');
+// console.log(
+//   "Aby nie wywalało błędu wszystkie informacje do modulo muszą być wyświetlane jako komentarz"
+// );
